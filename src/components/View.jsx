7834387089d6afefbee582ecba572de5a2641e5f
Karson Nichols <nicholsk18@gmail.com>
2021-01-08 13:37:51 -0600

    remade the app to use temple create react app with redux

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './View.css'
 
 const View = ({ name }) => {
     return (
@@ -7,48 +8,7 @@ const View = ({ name }) => {
                 <h2>
                     { name }
                 </h2>
-                {/* 
-                    Should we generate session from data you get here?
-                    Stay away from the data heavy sides of things for now
-                */}
-                <div className="view-sortables">
-                    <div>
-                        [Every][Friday]
-                    </div>
-                    <div>
-                        [After][Work]
-                    </div>
-                </div>
-
-                <div className="view-sortables">
-                    <div>
-                        [on the][02/03/21]
-                    </div>
-                    <div>
-                        [Start at][7:00]
-                    </div>
-                </div>
             </div>
-
-            <div className="view-component">
-                <h2>
-                    Sessions
-                </h2>
-                <div className="view-sortables">
-                    1/1/20
-                </div>
-                <div className="view-sortables">
-                    1/8/20
-                </div>
-                <div className="view-sortables">
-                    1/15/20
-                </div>
-
-                <div>
-                    <button className="btn">Load More</button>
-                </div>
-            </div>
-
         </>
     )
 }
