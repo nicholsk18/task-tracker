9727f7ca4de419048e738b6cbdb542b5e460e429
Karson Nichols <nicholsk18@gmail.com>
2021-01-14 10:13:32 -0600

    added useEffect & changed the way all activities are displaid

--- src/features/allActivities/ViewAllActivities.js
+++ src/features/allActivities/ViewAllActivities.js
@@ -1,7 +1,8 @@
 //https://reactjs.org/docs/lists-and-keys.html
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { ViewActivity } from "../activity/ViewActivity";
+import ActivityListItem from "../../components/ActivityListItem"
 import {
     BrowserRouter,
     Link,
@@ -23,25 +24,25 @@ export function ViewAllActivities() {
 
     // This needs to be useEffect. Can pass in []
     // eg
-    // useEffect(()=>{
-    //     dispatch(fetchAllActivities())
-    // }, [])
-    if (!loaded) {
+    useEffect(()=>{
         dispatch(fetchAllActivities())
-    }
+    }, [])
+    // if (!loaded) {
+    //     dispatch(fetchAllActivities())
+    //     console.log('test')
+    // }
 
     // Much nicer to use the object name
     const allActivities = activities.map((activity) =>
-            <div key={id} className={style.activity_container}>
+            <div key={activity.id} className={style.activity_container}>
                 <Link to={`/view/${activity.id}`}>
-                    {activity.name}
+                    <ActivityListItem name={activity.name} />
                 </Link>
-                {/* We should make this into a <ActivityListItem/> component */}
 
                 {/* This react router code doesn't belong in this file, and will be hurting the clarity of other concepts */}
                 <Switch>
-                    <Route path={`/view/${id}`}>
-                        <ViewActivity id={id} />
+                    <Route path={`/view/${activity.id}`}>
+                        <ViewActivity id={activity.id} />
                     </Route>
                 </Switch>
             </div>
