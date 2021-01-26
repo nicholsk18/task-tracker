3db3bf4888d8c7ca3b21da557a7fef6f09288f99
Karson Nichols <nicholsk18@gmail.com>
2021-01-25 20:36:42 -0600

    working on view sortable fragment

--- src/features/sortable/ViewSortableListFragment.js
+++ src/features/sortable/ViewSortableListFragment.js
@@ -1,23 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchSortableList, selectSortableList } from './sortableListSlice';
+import {
+  fetchSortableList,
+  selectSortableList,
+  isLoading,
+} from './sortableListSlice';
 
 const ViewSortableListFragment = ({ sortableId }) => {
   const sortableList = useSelector(selectSortableList);
+  const loading = useSelector(isLoading);
   const dispatch = useDispatch();
 
   const [id, setId] = useState(sortableId);
+  const [list, setList] = useState([]);
 
   useEffect(() => {
     dispatch(fetchSortableList(id));
   }, [id, dispatch]);
+
+  if (loading === 'loaded') {
+    console.log(sortableList);
+  }
+
+  useEffect(() => {
+    setList(sortableList);
+  }, [setList, sortableList]);
+
   return (
     <div>
       {/* call a sortable fragment here */}
-      <p>{sortableList.id}</p>
-      <p>
-        [{sortableList.one}][{sortableList.two}]
-      </p>
+      inside
+      {list.length > 0 && <div>{/* {list[id].one} {list[id].two} */}</div>}
     </div>
   );
 };
--- src/features/sortable/sortableListSlice.js
+++ src/features/sortable/sortableListSlice.js
@@ -1,35 +1,55 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const fetchSortableList = createAsyncThunk(
+  'list/sortableListFragmentStatus',
+  async (id) =>
+    await fetch(`http://localhost:3001/sortable/${id}`)
+      .then((res) => res.json())
+      .catch((error) => {
+        console.log(error);
+      })
+);
 
 const sortableListSlice = createSlice({
   name: 'sortableList',
   initialState: {
-    sortableList: {
-      id: '',
-      one: '',
-      two: '',
-    },
+    status: 'idle',
+    list: [],
   },
   reducers: {
-    setSortableList: (state, action) => {
-      state.sortableList = action.payload;
+    // setSortableList: (state, action) => {
+    //   console.log(action.payload);
+    //   state.sortableList = [action.payload];
+    // },
+  },
+  extraReducers: {
+    [fetchSortableList.pending]: (state, action) => {
+      state.status = 'loading';
+    },
+    [fetchSortableList.fulfilled]: (state, action) => {
+      state.status = 'loaded';
+      state.list.push(action.payload);
     },
   },
 });
 
-export const { setSortableList } = sortableListSlice.actions;
+// export const { setSortableList } = sortableListSlice.actions;
 
-export const fetchSortableList = (id) => (dispatch) => {
-  fetch(`http://localhost:3001/sortable/${id}`)
-    .then((res) => res.json())
-    .then((json) => {
-      // how to save this in the slice
-      dispatch(setSortableList({ id: json.id, one: json.one, two: json.two }));
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+// export const fetchSortableList = (id) => (dispatch) => {
+//   fetch(`http://localhost:3001/sortable/${id}`)
+//     .then((res) => res.json())
+//     .then((json) => {
+//       dispatch(setSortableList({ id: json.id, one: json.one, two: json.two }));
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// };
+
+export const selectSortableList = (state) => state.sortableList.list;
+export const isLoading = (state) => {
+  // console.log(state.sortableList);
+  return state.sortableList.status;
 };
-
-export const selectSortableList = (state) => state.sortableList.sortableList;
 
 export default sortableListSlice.reducer;
