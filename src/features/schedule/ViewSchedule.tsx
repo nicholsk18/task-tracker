1c4147d82546a1ad53191e131746368c7306c188
Karson Nichols <nicholsk18@gmail.com>
2021-02-01 13:28:32 -0600

    refactored schedule to not use activity or session

--- src/features/schedule/ViewSchedule.tsx
+++ src/features/schedule/ViewSchedule.tsx
@@ -3,11 +3,10 @@ import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { Card, Box } from '@material-ui/core';
 import { fetchSchedule, selectSchedule } from './scheduleSlice';
-import { fetchSession, selectSession } from '../session/sessionSlice';
 import ViewSortableListFragment from '../sortable/ViewSortableListFragment';
-import { fetchActivity, selectActivity } from '../activity/activitySlice';
 import Loading from '../../components/Loading';
 import ViewSessionFragment from '../session/ViewSessionFragment';
+import ViewActivityFragment from '../activity/ViewActivityFragment';
 
 export interface IUseParams {
   id: string;
@@ -15,39 +14,25 @@ export interface IUseParams {
 
 const ViewSchedule = () => {
   const dispatch = useDispatch();
-  const session = useSelector(selectSession);
   const schedule = useSelector(selectSchedule);
-  const activity = useSelector(selectActivity);
   const [params, setParams] = useState<IUseParams>(useParams());
-  const [sessionState, setSessionState] = useState(undefined);
 
   useEffect(() => {
     const { id } = params;
-    dispatch(fetchSession(id));
-    if (sessionState) {
-      dispatch(fetchActivity(session.activityId));
-      dispatch(fetchSchedule(session.scheduleId));
-    }
-  }, [params, dispatch, sessionState, session.activityId, session.scheduleId]);
-
-  // how do you do this cleaner?
-  if (session.sessionId !== 0 && !sessionState) {
-    setSessionState(session);
-  }
+    dispatch(fetchSchedule(id));
+  }, [params, dispatch]);
 
   // wait till all the ids are fetched before displaying
-  if (activity.id === 0 || schedule.id === 0) {
+  if (schedule.id === 0) {
     return <Loading />;
   }
 
   return (
     <Box mx='auto' my={3} maxWidth='450px'>
       <h2>View Schedule Screen</h2>
-      <Box my={3}>
-        <Card variant='outlined'>
-          <h3>{activity.name}</h3>
-        </Card>
-      </Box>
+
+      <ViewActivityFragment activityId={schedule.id} />
+
       <Box my={3}>
         <Card variant='outlined'>
           <h3>Sortables</h3>
