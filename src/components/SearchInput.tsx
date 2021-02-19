c2ee42651b335ed1da1f1b5a8095cd56979c3b97
Karson Nichols <nicholsk18@gmail.com>
2021-02-19 11:19:08 -0600

    deleted uneeded files

--- src/components/SearchInput.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FunctionComponent } from 'react';
-
-const SearchInput: FunctionComponent = () => {
-  return <div>Search input</div>;
-};
-
-export default SearchInput;
--- src/components/SearchLabel.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FunctionComponent } from 'react';
-
-const SearchLabel: FunctionComponent = () => {
-  return <div>Search Label</div>;
-};
-
-export default SearchLabel;
