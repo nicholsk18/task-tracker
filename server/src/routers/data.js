c607cf9a044503e12f3ca9a2d09587f7ed4fcf3f
Karson Nichols <nicholsk18@gmail.com>
2021-02-26 10:10:58 -0600

    format and added add relationship functionality

--- server/src/routers/data.js
+++ server/src/routers/data.js
@@ -72,4 +72,42 @@ router.delete('/remove/object', (req, res) => {
   res.send(objectData)
 })
 
+router.post('/get/type', (req, res) => {
+  const { relatesTo, type } = req.body.data
+  const relationships = []
+
+  for (const prop in DataTable) {
+    if (DataTable[prop].type === type) {
+      const obj = {
+        "id": prop,
+        "type": type,
+        "name": DataTable[prop].data.name
+      }
+      relationships.push(obj)
+    }
+  }
+
+  res.send(relationships)
+})
+
+router.post('/add/relationship', (req, res) => {
+  const { relatesTo, rel } = req.body.data
+
+  const fromID = relatesTo.toString()
+  const toID = rel.id.toString()
+
+  const keys = Object.keys(Relationships)
+  const lastID = parseInt(keys[keys.length-1])
+  const newID = lastID + 1
+
+  if (rel.type == 'Activity') {
+    Relationships[newID] = { "fromID": toID, "toID": fromID }
+  }
+  if (rel.type == 'Tag') {
+    Relationships[newID] = { "fromID": fromID, "toID": toID }
+  }
+
+  res.send({id: relatesTo})
+})
+
 module.exports = router
