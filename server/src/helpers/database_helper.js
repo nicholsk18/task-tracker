7d58e612241e527a8efb93aa6988be475c2681a9
Karson Nichols <nicholsk18@gmail.com>
2021-04-02 15:18:53 -0500

    rewrote how object is saved and how it is found inside of db

--- server/src/helpers/database_helper.js
+++ server/src/helpers/database_helper.js
@@ -4,32 +4,25 @@ databaseData = new TinyDB('./database.db');
 
 // right now either template or objects
 const getPart = (objectName) => {
-  return databaseData._data.data.find(
-    (object) => object[objectName] !== undefined
-  )[objectName];
+  return databaseData._data.data.find((object) => object.type === objectName);
 };
 
 // return just the template object data
 const getObjectTemplate = () => {
-  return getPart('Templates');
+  return databaseData._data.data.find((object) => object.type === 'Template');
+};
+
+const getAllObjectIDs = () => {
+  return getPart('objects').map((object) => object.id);
 };
 
 // return object based on ID
 const getObjectById = (id) => {
-  // get objects from an Object?
-  const objects = getPart('objects');
-  return objects.find((object) => object.id === id);
-};
-
-// helper function to get related objects
-const getObjectsByType = (type) => {
-  const objects = getPart('objects');
-  return objects.filter((object) => object.type === type);
+  return databaseData._data.data.find((object) => object.id === id);
 };
 
 // get all the object relationships
-const getRelationships = (to, ids) => {
-  const objects = getObjectsByType(to);
+const getRelationshipObjects = (ids) => {
   return ids.map((id) => {
     // should we remove relationship relationships?
     const { relationships, ...data } = getObjectById(id);
@@ -40,75 +33,50 @@ const getRelationships = (to, ids) => {
 const buildObject = (object, template) => {
   // object relationship ids
   const relationshipIDs = object['relationships'];
-
-  // help to get right part of template
-  const objectType = object['type'];
-
-  // type that objects needs relationships from
-  const relationshipTo = template[objectType].relationships.to;
-
   // lets get all the relationships
-  const relationships = getRelationships(relationshipTo, relationshipIDs);
-
+  const relationships = getRelationshipObjects(relationshipIDs);
   // lets not mutate the object
-  const { id, type, name } = object;
+  const { id, type, name, _id } = object;
   // return build object
-  return { id, type, name, relationships };
+  return { id, type, name, relationships, _id };
 };
+
+/**
+ *  Main functions below
+ */
 
 const getObject = (id) => {
   const template = getObjectTemplate();
   const object = getObjectById(id);
   const readyObject = buildObject(object, template);
   // send object and template
-  return { Template: template, object: readyObject };
+  const templatePart = template[readyObject.type];
+  return { Template: templatePart, data: readyObject };
 };
 
-//*** OLD STUFF BELOW ***//
+const saveObject = (object) => {
+  // lets rebuild the object with just relationship ids
+  const relationships = object.data.relationships.map((object) => object.id);
+  const newObject = { ...object.data };
+  newObject.relationships = relationships;
+  const id = newObject._id;
+  databaseData.findByIdAndRemove(id);
 
-// const getRelationships = (relationshipType) => {
-//   // this is fine for now
-//   const type = relationshipType === 'Tags' ? 'Tag' : 'Activity';
-//   const relationships = []; // what we will send back
-//
-//   databaseData.forEach((err, object) => {
-//     if (err) {
-//       console.log(err);
-//       return;
-//     }
-//
-//     if (object.Template.type == type) {
-//       relationships.push({ id: object.id, name: object.fields.name });
-//     }
-//   });
-//
-//   return relationships;
-// };
-//
-// const saveObject = (object) => {
-//   const _id = getGuidId(object.id);
-//   const index = databaseData.findByIdAndRemove(_id, (err, item, idx) => {
-//     if (err) {
-//       return err;
-//     }
-//
-//     return idx;
-//   });
-//
-//   // lets put it back from where we got it
-//   databaseData.insertItem(object, index);
-// };
+  databaseData.insertItem(newObject);
+};
 
 module.exports = {
   getObject,
   // getRelationships,
-  // saveObject,
+  saveObject,
 };
 
 // way for me to recreate data if needed
 const populate = () => {
-  initData.forEach((object) => {
+  databaseData.insertItem(initData[0]['Templates']);
+  initData[1]['objects'].forEach((object) => {
     databaseData.insertItem(object);
-    databaseData.flush();
   });
+
+  databaseData.flush();
 };
