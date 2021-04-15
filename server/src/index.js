93eab29107680ed9b3805e555cbd9452935448c3
Karson Nichols <nicholsk18@gmail.com>
2021-04-15 12:51:33 -0500

    heroku port

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 // const port = process.env.PORT || 3001
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
