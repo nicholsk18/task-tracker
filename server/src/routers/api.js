2ad9105a82c4468bbae8b1a6e6d9eb6c20c98295
Karson Nichols <nicholsk18@gmail.com>
2021-04-22 12:32:42 -0500

    added /api/ to server

--- server/src/routers/api.js
+++ server/src/routers/api.js
@@ -10,31 +10,31 @@ const {
   deleteObject,
 } = require('../helpers/database_helper');
 
-// router.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../../build/index.html'));
-// });
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../build/index.html'));
+});
 
-router.get('/object/:id', (req, res) => {
+router.get('/api/object/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const dataObject = getObject(id);
   res.send(dataObject);
 });
 
-router.post('/get/relationships', (req, res) => {
+router.post('/api/get/relationships', (req, res) => {
   const type = req.body.data;
   const relationships = getRelationships(type);
 
   res.send(relationships);
 });
 
-router.post('/get/template', (req, res) => {
+router.post('/api/get/template', (req, res) => {
   const type = req.body.data;
   const template = getTemplate(type);
 
   res.send(template);
 });
 
-router.post('/save/object', (req, res) => {
+router.post('/api/save/object', (req, res) => {
   const newObject = req.body.data;
 
   const obj = saveObject(newObject);
@@ -42,14 +42,14 @@ router.post('/save/object', (req, res) => {
   res.send(obj);
 });
 
-router.post('/create/object', (req, res) => {
+router.post('/api/create/object', (req, res) => {
   const obj = req.body.data;
   console.log(obj);
   const newObj = createObject(obj);
   res.send(newObj);
 });
 
-router.delete('/delete/object', (req, res) => {
+router.delete('/api/delete/object', (req, res) => {
   const _id = req.body.data;
   const err = deleteObject(_id);
 
